d4e712bb974b3f379933ab6517643a14661bb568
carbidx2025 <carbidx2025@gmail.com>
2025-11-13 01:35:28 -0800

    Create topbar.jsx

---
+++ frontend/src/components/topbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./TopBar.css";
+
+export default function TopBar() {
+  return (
+    <div className="topbar">
+      <span className="topbar-title">Welcome to CarBidX</span>
+    </div>
+  );
+}
+
