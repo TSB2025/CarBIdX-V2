55f19f693d84ef2077719e2e3a303f05be35310f
carbidx2025 <carbidx2025@gmail.com>
2025-11-13 18:46:40 -0800

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,1 +1,19 @@
-import React from 'react'; export default function Navbar(){return(<div></div>);} 
+import { Link } from "react-router-dom";
+import "./../styles/layout.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">
+        <Link to="/">CarBidX V2</Link>
+      </div>
+
+      <ul className="nav-links">
+        <li><Link to="/buyer/request">Buyers</Link></li>
+        <li><Link to="/dealer/login">Dealers</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
