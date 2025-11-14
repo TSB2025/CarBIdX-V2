3e0e24117fd71d0b1108db6da61590221a3b6546
carbidx2025 <carbidx2025@gmail.com>
2025-11-13 18:47:57 -0800

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,1 +1,11 @@
-import React from 'react'; export default function Footer(){return(<div></div>);} 
+import "./../styles/layout.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} CarBidX V2. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
