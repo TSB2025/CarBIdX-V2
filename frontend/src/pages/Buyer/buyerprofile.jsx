22337a24a43d5876f3317267497d8e2ee189f4cc
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 22:04:14 -0800

    Create buyerprofile.jsx

---
+++ frontend/src/pages/Buyer/buyerprofile.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BuyerProfile() {
+  return (
+    <div style={{ padding: "30px", color: "white" }}>
+      <h1>Profile & Settings</h1>
+      <p>Manage your buyer account settings here.</p>
+    </div>
+  );
+}
