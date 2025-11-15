6a6c6bad5916851122a250711ce0d0882dca2422
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 22:19:47 -0800

    Create adminmaintenance.jsx

---
+++ frontend/src/pages/Admin/adminmaintenance.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminMaintenance() {
+  return (
+    <div style={{ padding: "30px", color: "white" }}>
+      <h1>Maintenance Mode</h1>
+      <p>Maintenance mode is currently disabled.</p>
+    </div>
+  );
+}
