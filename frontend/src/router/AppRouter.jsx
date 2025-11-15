18f05fc565373371c8cbb1e84e64bcbc04f943e9
carbidx2025 <carbidx2025@gmail.com>
2025-11-15 02:39:58 -0800

    Update AppRouter.jsx

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -7,7 +7,7 @@ import DealerLayout from "../layouts/DealerLayout";
 import AdminLayout from "../layouts/AdminLayout";
 
 /* Public Pages */
-import home from "../frontend/src/pages/home";
+import home from "../pages/home";
 
 /* Buyer Pages */
 import BuyerDashboard from "../pages/Buyer/buyerdashboard";
