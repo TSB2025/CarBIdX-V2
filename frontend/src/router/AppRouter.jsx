7673ffce6130b6ecff2f3efbbb1abf3bb98b2112
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 18:42:17 -0800

    Update AppRouter.jsx

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,1 +1,53 @@
-export default function AppRouter(){return null}
+import { Routes, Route } from "react-router-dom";
+
+/* Public Pages */
+import Home from "../pages/home";
+
+/* Buyer Pages */
+import BuyerLogin from "../pages/Buyer/BuyerLogin";
+import BuyerRegister from "../pages/Buyer/BuyerRegister";
+import BuyerVerify from "../pages/Buyer/BuyerVerify";
+import BuyerConfirm from "../pages/Buyer/BuyerConfirm";
+import BuyerDashboard from "../pages/Buyer/Dashboard";
+import BuyerAuctions from "../pages/Buyer/Auctions";
+import BuyerAuctionDetail from "../pages/Buyer/AuctionDetail";
+
+/* Dealer Pages */
+import DealerLogin from "../pages/Dealer/DealerLogin";
+import DealerDashboard from "../pages/Dealer/Dashboard";
+import DealerCreateListing from "../pages/Dealer/CreateListing";
+import DealerMyListings from "../pages/Dealer/MyListings";
+
+/* Admin Pages */
+import AdminLogin from "../pages/Admin/Login";
+import AdminDashboard from "../pages/Admin/Dashboard";
+
+export default function AppRouter() {
+  return (
+    <Routes>
+
+      {/* Public */}
+      <Route path="/" element={<Home />} />
+
+      {/* Buyer */}
+      <Route path="/buyer/login" element={<BuyerLogin />} />
+      <Route path="/buyer/register" element={<BuyerRegister />} />
+      <Route path="/buyer/verify" element={<BuyerVerify />} />
+      <Route path="/buyer/confirm" element={<BuyerConfirm />} />
+      <Route path="/buyer/dashboard" element={<BuyerDashboard />} />
+      <Route path="/buyer/auctions" element={<BuyerAuctions />} />
+      <Route path="/buyer/auction/:id" element={<BuyerAuctionDetail />} />
+
+      {/* Dealer */}
+      <Route path="/dealer/login" element={<DealerLogin />} />
+      <Route path="/dealer/dashboard" element={<DealerDashboard />} />
+      <Route path="/dealer/create-listing" element={<DealerCreateListing />} />
+      <Route path="/dealer/listings" element={<DealerMyListings />} />
+
+      {/* Admin */}
+      <Route path="/admin/login" element={<AdminLogin />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
+
+    </Routes>
+  );
+}
