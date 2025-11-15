b7563f25e0a9f766843c2eec5442988572f82f60
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 22:10:53 -0800

    Create dealerbids.jsx

---
+++ frontend/src/pages/Dealer/dealerbids.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DealerBids() {
+  return (
+    <div style={{ padding: "30px", color: "white" }}>
+      <h1>My Bids</h1>
+      <p>No bids submitted.</p>
+    </div>
+  );
+}
