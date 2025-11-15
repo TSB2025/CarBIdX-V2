c7403be890e5d8d4b1ffd47f18355649abda3577
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 21:58:59 -0800

    Update AppRouter.jsx

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,52 +1,81 @@
+// PATH: frontend/src/router/AppRouter.jsx
+
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-/* Public Pages */
+/* Home */
 import Home from "../pages/home";
 
-/* Buyer Pages */
-import BuyerLogin from "../pages/Buyer/BuyerLogin";
-import BuyerRegister from "../pages/Buyer/BuyerRegister";
-import BuyerVerify from "../pages/Buyer/BuyerVerify";
-import BuyerConfirm from "../pages/Buyer/BuyerConfirm";
-import BuyerDashboard from "../pages/Buyer/Dashboard";
-import BuyerAuctions from "../pages/Buyer/Auctions";
-import BuyerAuctionDetail from "../pages/Buyer/AuctionDetail";
+/* Buyer pages */
+import BuyerLogin from "../pages/Buyer/buyerlogin";
+import BuyerRegister from "../pages/Buyer/buyerregister";
+import BuyerVerify from "../pages/Buyer/buyerverify";
+import BuyerDashboard from "../pages/Buyer/buyerdashboard";
+import BuyerAuctions from "../pages/Buyer/buyerauctions";
+import BuyerAuctionDetail from "../pages/Buyer/buyerauctiondetail";
+import BuyerConfirm from "../pages/Buyer/buyerconfirm";
+import BuyerRequests from "../pages/Buyer/buyerrequests";
+import BuyerProfile from "../pages/Buyer/buyerprofile";
 
-/* Dealer Pages */
-import DealerLogin from "../pages/Dealer/DealerLogin";
-import DealerDashboard from "../pages/Dealer/Dashboard";
-import DealerCreateListing from "../pages/Dealer/CreateListing";
-import DealerMyListings from "../pages/Dealer/MyListings";
+/* Dealer pages */
+import DealerLogin from "../pages/Dealer/dealerlogin";
+import DealerDashboard from "../pages/Dealer/dealerdashboard";
+import DealerRequests from "../pages/Dealer/dealerrequests";
+import DealerBids from "../pages/Dealer/dealerbids";
+import DealerResults from "../pages/Dealer/dealerresults";
+import DealerProfile from "../pages/Dealer/dealerprofile";
 
-/* Admin Pages */
-import AdminLogin from "../pages/Admin/Login";
-import AdminDashboard from "../pages/Admin/Dashboard";
+/* Admin pages */
+import AdminLogin from "../pages/Admin/adminlogin";
+import AdminDashboard from "../pages/Admin/admindashboard";
+import AdminApproveDealers from "../pages/Admin/adminapprovedealers";
+import AdminSuspendDealers from "../pages/Admin/adminsuspenddealers";
+import AdminSuspendBuyers from "../pages/Admin/adminsuspendbuyers";
+import AdminDepositSettings from "../pages/Admin/admindeposits";
+import AdminMaintenance from "../pages/Admin/adminmaintenance";
+import AdminMFA from "../pages/Admin/adminmfa";
+
+/* Global */
+import Logout from "../pages/logout";
 
 export default function AppRouter() {
   return (
     <Routes>
 
-      {/* Public */}
+      {/* HOME */}
       <Route path="/" element={<Home />} />
 
-      {/* Buyer */}
+      {/* BUYER ROUTES */}
       <Route path="/buyer/login" element={<BuyerLogin />} />
       <Route path="/buyer/register" element={<BuyerRegister />} />
       <Route path="/buyer/verify" element={<BuyerVerify />} />
-      <Route path="/buyer/confirm" element={<BuyerConfirm />} />
       <Route path="/buyer/dashboard" element={<BuyerDashboard />} />
       <Route path="/buyer/auctions" element={<BuyerAuctions />} />
       <Route path="/buyer/auction/:id" element={<BuyerAuctionDetail />} />
+      <Route path="/buyer/confirm" element={<BuyerConfirm />} />
+      <Route path="/buyer/requests" element={<BuyerRequests />} />
+      <Route path="/buyer/profile" element={<BuyerProfile />} />
 
-      {/* Dealer */}
+      {/* DEALER ROUTES */}
       <Route path="/dealer/login" element={<DealerLogin />} />
       <Route path="/dealer/dashboard" element={<DealerDashboard />} />
-      <Route path="/dealer/create-listing" element={<DealerCreateListing />} />
-      <Route path="/dealer/listings" element={<DealerMyListings />} />
+      <Route path="/dealer/requests" element={<DealerRequests />} />
+      <Route path="/dealer/bids" element={<DealerBids />} />
+      <Route path="/dealer/results" element={<DealerResults />} />
+      <Route path="/dealer/profile" element={<DealerProfile />} />
 
-      {/* Admin */}
-      <Route path="/admin/login" element={<AdminLogin />} />
+      {/* ADMIN ROUTES */}
+      <Route path="/securecmb" element={<AdminLogin />} />
       <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      <Route path="/admin/approve-dealers" element={<AdminApproveDealers />} />
+      <Route path="/admin/suspend-dealers" element={<AdminSuspendDealers />} />
+      <Route path="/admin/suspend-buyers" element={<AdminSuspendBuyers />} />
+      <Route path="/admin/deposits" element={<AdminDepositSettings />} />
+      <Route path="/admin/maintenance" element={<AdminMaintenance />} />
+      <Route path="/admin/mfa" element={<AdminMFA />} />
+
+      {/* LOGOUT */}
+      <Route path="/logout" element={<Logout />} />
 
     </Routes>
   );
