c6b1d762dcd489d26b34906b7552b08e8fec4a6b
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 22:09:56 -0800

    Create dealerrequests.jsx

---
+++ frontend/src/pages/Dealer/dealerrequests.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DealerRequests() {
+  return (
+    <div style={{ padding: "30px", color: "white" }}>
+      <h1>Live Buyer Requests</h1>
+      <p>No buyer requests available.</p>
+    </div>
+  );
+}
